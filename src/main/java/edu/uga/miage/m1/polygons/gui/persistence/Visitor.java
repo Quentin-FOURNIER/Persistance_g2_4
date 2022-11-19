e279a5f6929640c646681a2f857fb41bde37e1eb
l3miage-fournieq <quentin2807@free.fr>
2022-11-19 18:25:44 +0100

    Correction Visitor

--- src/main/java/edu/uga/miage/m1/polygons/gui/persistence/Visitor.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/persistence/Visitor.java
@@ -1,19 +1,15 @@
 package edu.uga.miage.m1.polygons.gui.persistence;
 
-import edu.uga.miage.m1.polygons.gui.shapes.BaseShape;
 import edu.uga.miage.m1.polygons.gui.shapes.Circle;
 import edu.uga.miage.m1.polygons.gui.shapes.Square;
 import edu.uga.miage.m1.polygons.gui.shapes.Triangle;
 
-/**
- * You must define a method for each type of Visitable.
- * @author <a href="mailto:christophe.saint-marcel@univ-grenoble-alpes.fr">Christophe</a>
- */
+
 public interface Visitor {
 
-    public void visit(Circle circle);
+    void visit(Circle circle);
 
-    public void visit(Square square);
+    void visit(Square square);
 
-    public void visit(Triangle triangle);
+    void visit(Triangle triangle);
 }
