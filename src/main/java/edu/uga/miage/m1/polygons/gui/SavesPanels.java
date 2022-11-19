15a8802327d722f4d5de92b640972acf5f0f03f9
l3miage-gueissae <emmanuel.gueissaz@gmail.com>
2022-11-19 18:58:52 +0100

    SimpleFactory for create shape

--- src/main/java/edu/uga/miage/m1/polygons/gui/SavesPanels.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/SavesPanels.java
@@ -22,25 +22,22 @@ public class SavesPanels {
         index = 0;
     }
 
-    public void addPanel(ArrayList<BaseShape> panel) throws IOException {
+    public void addPanel(List<BaseShape> panel) throws IOException {
 
         List<BaseShape> listBaseShape = new ArrayList<>();
 
         BaseShape tmpBaseShape;
         BaseShape tmpShape = null;
 
-        System.out.println("PANEL > " + panel);
-        System.out.println("AVANT > " + saves);
+
         while (saves.size() > index + 1) {
             saves.remove(index+1);
         }
-        System.out.println("APRES > " + saves);
 
         for (BaseShape baseShape: panel) {
             tmpBaseShape = new BaseShape();
 
             for (BaseShape shape: baseShape.getShapes()) {
-                System.out.println("THE SHAPE +> " + shape);
 
                 switch (shape.getName()) {
                     case "Triangle" -> tmpShape = new Triangle(shape.getX() + 25, shape.getY() + 25);
@@ -52,13 +49,11 @@ public class SavesPanels {
             }
             listBaseShape.add(tmpShape);
             tmpBaseShape.getShapes().add(tmpShape);
-            //System.out.println("TMPSHAPE : " + tmpShape.getShapes());
             if (tmpShape != null)
                 tmpBaseShape.add(tmpShape);
         }
 
         saves.add(listBaseShape);
-        System.out.println(index + "--> " + saves + " <--");
 
         index++;
     }
@@ -67,9 +62,7 @@ public class SavesPanels {
         if (index >= 1) {
             index--;
             panel.removeAll();
-            System.out.println("SAVES " + saves.get(index));
             for (BaseShape bs : saves.get(index)) {
-                System.out.println("New -> " + bs);
                 panel.add(bs);
             }
             panel.repaint();
@@ -77,12 +70,10 @@ public class SavesPanels {
     }
 
     public void redo(JPanel panel) {
-        System.out.println(index + "--> " + saves + " <-- REDO");
         if (index < saves.size() -1) {
             index++;
             panel.removeAll();
             for (BaseShape bs : saves.get(index)) {
-                System.out.println("New -> " + bs);
                 panel.add(bs);
             }
             panel.repaint();
